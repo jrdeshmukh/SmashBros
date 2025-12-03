3b38c04e5c60e5c69028079fef312ed9cc5da3d8
akhilketty <akhilketty@gmail.com>
2025-12-03 14:59:03 -0500

    Player Select Start

--- SmashBros/Assets/Scripts/MiniPekkaSelect.cs
+++ SmashBros/Assets/Scripts/MiniPekkaSelect.cs
@@ -1,21 +1,28 @@
+using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
-
-public class SceneSelectorForest : MonoBehaviour
+public class MiniPekkaSelect : MonoBehaviour
 {
-    public Button yourButton; 
+    public Button yourButton;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         Button btn = yourButton.GetComponent<Button>();
         btn.onClick.AddListener(OnButtonClick);
     }
+
     public void OnButtonClick()
         {
-        SceneManager.LoadScene("Map 2");
-        Debug.Log("Forest");
+        if (Input.GetKey("1"))
+        {
+            Debug.Log("1 = Mini Pekka");
+        }
+
+        if (Input.GetKey("2"))
+        {
+            Debug.Log("2 = Mini Pekka");
+        }
     }
     // Update is called once per frame
     void Update()
---
+++ SmashBros/Assets/Scripts/PlayerSelect.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class PlayerSelect : MonoBehaviour
+{
+    public int miniPekka;
+    public int skeleton;
+    public int valkyrie;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
